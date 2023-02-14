9bb8858c8bf3bb65a924ba6b78943b13855f004b
Felipe Santiago Gama <felipes4ntiago@hotmail.com>
2023-02-14 00:44:13 -0300

    modificando o app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Atividade GitHub Actions- novo workflow
+         Atividade GitHub Actions- branch dev
         </p>
         <a
           className="App-link"
